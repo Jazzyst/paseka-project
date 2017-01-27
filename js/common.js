2dcf19d9831132cc44b1912850bdb0e89cbb2026
Jazzyst <andriy.zhuravlov@gmail.com>
2017-01-27 15:57:50 +0200

    version2.0

--- js/common.js
+++ js/common.js
@@ -1,51 +1,110 @@
 $(document).ready(function() {
 
+
+
+	
+
+	$(".toggle_mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		$(".sandwich").toggleClass("active");
+	});
+
+
+// 	$(function() {
+//     //here you have the control over the body of the iframe document
+//     var iBody = $("#frame").contents().find("body");
+//     //here you have the control over any element (#myContent)
+//     var myContent = iBody.find("#myContent");
+
+// });
+
+
 	$(".rslides").responsiveSlides({
 				auto: true,
 				pager: true,
 				speed: 300,
-				maxwidth: 700
+				maxwidth: 700,
+				
+
 	});
-	
+		$(".button").magnificPopup();
+		$(".registration").magnificPopup();
 
-	$('#portfolio_grid').mixItUp();
-
-	$(".s_portfolio li").click(function(){
-		$(".s_portfolio li").removeClass("active");
-		$(this).addClass("active");
+		$("#form").submit(function() { //Change
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за  Вашу заявку!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+			}, 500);
+		});
+		return false;
 	});
 
+		$("#form-reg").submit(function() { //Change
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: $(this).serialize()
+		}).done(function() {
+			alert("В ближайшее время мы с Вами свяжемся!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+			}, 500);
+		});
+		return false;
+	});
+
+	// $('#portfolio_grid').mixItUp();
+
+	// $(".s_portfolio li").click(function(){
+	// 	$(".s_portfolio li").removeClass("active");
+	// 	$(this).addClass("active");
+	// });
+	$(".item").magnificPopup({
+		type : "image",
+		gallery : {
+			enabled : true
+		},
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
+	});
 
 
 	$(".popup").magnificPopup({type:"image"});
 	$(".popup_content").magnificPopup({type:"inline", midClick: true});
 
-	$(".animation_1").animated("flipInY", "flipOutY");
-	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
-	$(".animation_3").animated("fadeInRight", "fadeOutRight");
 
-	$(".top_text img").animated("fadeInDown", "fadeOutUp");
-	$(".top_text h1, .section_header").animated("fadeInUp", "fadeOutDown");
+	// $(".animation_1").animated("flipInY", "flipOutY");
+	// $(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	// $(".animation_3").animated("fadeInRight", "fadeOutRight");
 
-	$(".left .news_item").animated("fadeInLeft", "fadeOutLeft");
-	$(".right .news_item").animated("fadeInRight", "fadeOutRight");
+	// $(".top_text img").animated("fadeInDown", "fadeOutUp");
+	// $(".top_text h1, .section_header").animated("fadeInUp", "fadeOutDown");
 
-	function heightDetect(){
-		$(".main_head").css("height", $(window).height());
-	};
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
+	// $(".left .news_item").animated("fadeInLeft", "fadeOutLeft");
+	// $(".right .news_item").animated("fadeInRight", "fadeOutRight");
 
-	$(".toggle_mnu").click(function() {
-		$(".sandwich").toggleClass("active");
-	});
+	// function heightDetect(){
+	// 	$(".main_head").css("height", $(window).height());
+	// };
+	// heightDetect();
+	// $(window).resize(function(){
+	// 	heightDetect();
+	// });
 
-	$(".top_mnu ul a").click(function(){
-		// $(".top_mnu").fadeOut(600);
-		$(".sandwich").toggleClass("active");
-	});
+	// $(".toggle_mnu").click(function() {
+	// 	$(".sandwich").toggleClass("active");
+	// });
+
+	// $(".top_mnu ul a").click(function(){
+	// 	// $(".top_mnu").fadeOut(600);
+	// 	$(".sandwich").toggleClass("active");
+	// });
 
 	// $(".toggle_mnu").click(function(){
 	// 	if ($(".top_mnu").is(":visible")){
@@ -65,9 +124,18 @@ $(document).ready(function() {
 
 	});
 
-	$("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+// 	$("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
 
-	$(".top_mnu ul a").mPageScroll2id();
+// 	$(".top_mnu ul a").mPageScroll2id();
+
+	var $container = $(".masonry-container");
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			columnWidth: ".item_img",
+			itemSelector: ".item_img"
+		});
+		$(".item_img").imagefill();
+	});
 
 });
 
@@ -75,3 +143,4 @@ $(window).load(function() {
 	$(".loader_inner").fadeOut(); 
 	$(".loader").delay(400).fadeOut("slow"); 
 });
+
